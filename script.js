b2d735bfd881b4c03b4c8038012a04d478e7577c
palomamaced <167140052+palomamaced@users.noreply.github.com>
2024-09-24 14:26:58 -0300

    adicionando o arquivo do projeto

---
+++ script.js
@@ -0,0 +1,106 @@
+const caixaPrincipal= document.querySelector('.caixa-caixaPrincipal');
+const caixaPerguntas =document .querySelector(".caixa-principal");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector("caixa-resultado");
+const textoResultado=document.querySelector("texto-resultado");s 
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+   perguntaAtual = perguntas[atual];
+ caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta() {
+ perguntaAtual = perguntas[atual];
+ caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraPergunta() {
+}
+mostraPergunta();
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+    
+mostraPergunta()
+        let atual = 0;
+        let perguntaAtual;
+        
+        function mostraPergunta() {
+            perguntaAtual = perguntas[atual];
+            caixaPerguntas.textContent = perguntaAtual.enunciado;
+            mostraAlternativas();
+        }
+        function mostraAlternativas() {
+            for(const alternativa of perguntaAtual.alternativas) {
+                const botaoAlternativas = document.createElement("button");
+                botaoAlternativa.textContent = alternativa;
+            
+            }
+        }
+        let atual = 0;
+        let perguntaAtual;
+        
+        function mostraPergunta() {
+            perguntaAtual = perguntas[atual];
+            caixaPerguntas.textContent = perguntaAtual.enunciado;
+            mostraAlternativas();
+        }
+        function mostraAlternativas() {
+            for(const alternativa of perguntaAtual.alternativas) {         const botaoAlternativas = document.createElement("button");
+     botaoAlternativa.textContent = alternativa;
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();                caixaAlternativas.appendChild(botaoAlternativas);
+            }
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+const alternativa
+(const alternativa of perguntaAtual.alternativas)
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+  }
+  function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+    }
+    function mostraAlternativas() {
+      for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+      }
+    }
+    function mostraAlternativas() {
+      for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+      }
+      enunciado: "Assim que saiu da escola voce depara com uma nova tecnologia, um chat que alternativas: [{    texto: "Isso assustador!",    afirmacao: "No  ficou com medo do que essa tecnologia pode fazer.{    texto: "Isso  maravilhoso!",    afirmacao: "Quis saber como usar IA no seu dia a dia."}
+      scrollY
